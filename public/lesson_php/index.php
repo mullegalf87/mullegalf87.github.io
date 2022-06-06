83b146be75b236735dc49f2ff0f3201f6aa7bfc0
mullegalf87 <dario.rosciglione87@gmail.com>
2022-06-06 14:03:11 +0200

    edit lesson_php

--- public/lesson_php/index.php
+++ public/lesson_php/index.php
@@ -3,11 +3,6 @@
     <head>
         <!-- dataresponsive -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- jquery 3.5 jquery-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <!-- bootstrap 4.5 bootstrap-->
-        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </head>
     <style>
         h6{
